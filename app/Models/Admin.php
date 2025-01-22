dc23528a62a1339d7bd00db4c68468601219053a
Nataly Baranova <borashek@inbox.ru>
2025-01-22 13:15:30 +0300

    feat: refactor admin model and publish filament config

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,70 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+/**
+ * @property string $email
+ */
+class Admin extends Authenticatable implements FilamentUser, HasName
 {
-    //
+    use Notifiable;
+
+    protected $table = 'admins';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Rule for access filament admin panel
+     * @param Panel $panel
+     * @return bool
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get admin email instead of filament name
+     * @return string
+     */
+    public function getFilamentName(): string
+    {
+        return  $this->email ?? "admin@admin.com";
+    }
 }
